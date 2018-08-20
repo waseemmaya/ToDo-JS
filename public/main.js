ca234de7ebc48b5ee84238c929ad17c127773d8f
Waseem Maya <waseemmayaa@gmail.com>
2018-08-21 02:58:18 +0500

    add delete option

--- public/main.js
+++ public/main.js
@@ -7,6 +7,7 @@ function addTodo() {
     alert("Sorry Enter Something");
   } else {
     tasks.push({
+      key: Math.floor(100444000 + Math.random() * 90012000),
       title: title,
       body: body,
       date: new Date().toDateString()
@@ -28,10 +29,41 @@ function showTasks() {
                             }</ul>
                             <ul class="list-group-item">Body : ${data.body}</ul>
                             <ul class="list-group-item">Time : ${data.date}</ul>
-                           <div class="container"> <button class="btn btn-primary  mb-2 mt-2">Delete</button></div>
+                           <div class="container"> 
+                           <button onClick="deleteTask(${data.key},'${
+      data.title
+    }')" class="btn btn-primary  mb-2 mt-2" data-toggle="modal" data-target="#myModal">
+                           Delete
+                           </button>
+                           </div>
                         </li>
                         <hr>
                     `;
   });
   document.getElementById("tasks").innerHTML = output;
 }
+
+function deleteTask(val, tit) {
+  console.log(tit);
+  var result = confirm(`Are you sure you want to delete?  
+  <h3>${tit}</h3>`);
+  if (result) {
+    //Logic to delete the item
+    for (var i = 0; i < tasks.length; i++)
+      if (tasks[i].key === val) {
+        tasks.splice(i, 1);
+        showTasks();
+        break;
+      }
+  }
+}
+
+// function deleteTask(val) {
+//   console.log(val);
+//   tasks.forEach(element => {
+//     console.log(element.key);
+//     if (val === element.key) {
+//       console.log("mil gaya");
+//     }
+//   });
+// }
